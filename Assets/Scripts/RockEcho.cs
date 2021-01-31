d48114cefaf14fba0b3899931a853e2376759545
myersca2024 <myers.ca@northeastern.edu>
2021-01-30 21:39:35 -0600

    Rock echo effect

---
+++ Assets/Scripts/RockEcho.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockEcho : MonoBehaviour
+{
+    public float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+    public GameObject echo;
+
+    void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            GameObject echoObj = Instantiate(echo, transform.position, Quaternion.identity);
+            Destroy(echoObj, 1f);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
