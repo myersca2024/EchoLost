7c517beb03609090f4c0acb4c6e51d2ac546a0df
juliasherbal <sherbaljulia@gmail.com>
2021-05-16 13:48:49 -0400

    update scene management

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -6,25 +6,20 @@ using UnityEngine.SceneManagement;
 public class ExitLevel : MonoBehaviour
 {
 
-    public string current;
+    public string nextLevel;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (string.Equals(current, "Level2"))
-            {
-                SceneManager.LoadScene("Level1");
-            }
-            else if (string.Equals(current, "Level1"))
-            {
-                SceneManager.LoadScene("Level3");
-            }
-            else if (string.Equals(current, "Level3"))
-            {
-                SceneManager.LoadScene("Game Over");
-            }
+            // play a sound
+            Invoke("LoadLevel", 1);
         }
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
     }
 
 
